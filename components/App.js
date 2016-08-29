605494f843f78269e95310baee26106d3db856e4
Michael Liu <michael.liu@getpantheon.com>
2016-08-28 20:40:50 -0700

    Images

--- components/App.js
+++ components/App.js
@@ -71,11 +71,72 @@ export default class App extends React.Component {
                 </ul>
             </div>
         </nav>
+</header>
+        <div id="main">
+            <div id="featured-content">
+                <div className="featured-post" id="featured-post-1" style={{opacity: 1, visibility: "visible"}}>
+                    <a href="https://everafterdemo.wordpress.com/2012/01/11/honeymoon-in-venice/" title="Permalink to Honeymoon in Venice" rel="bookmark">
+                        <img width="850" height="400" src={"https://everafterdemo.files.wordpress.com/2012/01/wedding-venice.jpg?w=850&amp;h=400&amp;crop=1"} className="attachment-large-feature size-large-feature wp-post-image" alt="wedding-venice" scale="0"/>
+                    </a>
+                    <div className="feature-content">
+                        <h1 className="feature-title">
+                            <a href="https://everafterdemo.wordpress.com/2012/01/11/honeymoon-in-venice/" rel="bookmark">Honeymoon in Venice</a>
+                        </h1>
+                    </div>
+                </div>
+                <div className="featured-post" id="featured-post-2" style={{opacity: 0, visibility: "hidden"}}>
+                    <a href="https://everafterdemo.wordpress.com/2012/01/11/a-single-white-rose/" title="Permalink to A Single White Rose" rel="bookmark">
+                        <img width="850" height="400" src={"https://everafterdemo.files.wordpress.com/2012/01/wedding-single-white-rose.jpg?w=850&amp;h=400&amp;crop=1"} className="attachment-large-feature size-large-feature wp-post-image" alt="wedding-single-white-rose" scale="0"/>
+                    </a>
+                    <div className="feature-content">
+                        <h1 className="feature-title">
+                            <a href="https://everafterdemo.wordpress.com/2012/01/11/a-single-white-rose/" rel="bookmark">A Single White Rose</a>
+                        </h1>
+                    </div>
+                </div>
+                <div className="featured-post" id="featured-post-3" style={{opacity: 0, visibility: "hidden"}}>
+                    <a href="https://everafterdemo.wordpress.com/2012/01/10/dress-shopping/" title="Permalink to Dress Shopping" rel="bookmark">
+                        <img width="850" height="400" src={"https://everafterdemo.files.wordpress.com/2012/01/wedding-dress-shopping.jpg?w=850&amp;h=400&amp;crop=1"} className="attachment-large-feature size-large-feature wp-post-image" alt="Dress Shopping" scale="0"/>
+                    </a>
+                    <div className="feature-content">
+                        <h1 className="feature-title">
+                            <a href="https://everafterdemo.wordpress.com/2012/01/10/dress-shopping/" rel="bookmark">Dress Shopping</a>
+                        </h1>
+                    </div>
+                </div>
+            </div>
+            <nav id="feature-slider">
+                <ul>
+                    <li>
+                        <a href="#featured-post-1" className="">Honeymoon in Venice</a>
+                    </li>
+                    <li>
+                        <a href="#featured-post-2" className="">A Single White Rose</a>
+                    </li>
+                    <li>
+                        <a href="#featured-post-3" className="active">Dress Shopping</a>
+                    </li>
+                </ul>
+            </nav>
 
-        <main>
-        {this.props.children}
-        </main>
-    </header>
+            <div role="main" className="post-104 post type-post status-publish format-standard sticky hentry category-books category-writing tag-announcement tag-panache">
+                    <main>
+                    {this.props.children}
+                    </main>
+           </div>
+                <div id="latest-message" className="format-quote">
+                    <div className="avatar">
+                        <img alt="" src="https://1.gravatar.com/avatar/4fdb3b572ac7dd8d7a58ba70317efa14?s=54&amp;d=identicon&amp;r=G" className="avatar avatar-54" height="54" width="54" originals="54" scale="2"/>
+                    </div>
+                    <div className="message-content">
+                        <blockquote>
+                            <p>A successful marriage requires falling in love many times, always with the same person.<br/>
+                                <cite>— Mignon McLaughlin</cite>
+                            </p>
+                        </blockquote>
+                    </div>
+                </div>
+        </div>
     </div>
       )
 }
